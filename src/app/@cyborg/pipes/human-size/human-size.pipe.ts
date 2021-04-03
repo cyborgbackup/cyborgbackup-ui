323339076f04be5667639526f9c763abd688c903
Gaëtan FEREZ <gaetan@ferez.fr>
2021-04-03 11:28:31 +0200

    Set human readable size on Catalog page

--- src/app/@cyborg/pipes/human-size/human-size.pipe.ts
+++ src/app/@cyborg/pipes/human-size/human-size.pipe.ts
@@ -8,6 +8,7 @@ export class HumanSizePipe implements PipeTransform {
   transform(value: any, args?: any): any {
     if (isNaN(value)) { return ''; }
     const size = parseFloat(value);
+    if (size <= 0) { return '0 B'; }
     const i = Math.floor( Math.log(size) / Math.log(1024) );
     const item: number =  size / Math.pow(1024, i);
     return item.toFixed(2) + ' ' + ['B', 'kB', 'MB', 'GB', 'TB'][i];
