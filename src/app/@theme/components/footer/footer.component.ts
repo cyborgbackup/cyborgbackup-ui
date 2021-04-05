a59d8aeda22243b8dd9daae16ea7f03610877a3b
Gaëtan FEREZ <gaetan@ferez.fr>
2021-04-05 11:33:56 +0200

    Fix copyright year

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'ngx-footer',
   styleUrls: ['./footer.component.scss'],
   template: `
-    <span class="created-by"><a href="https://cyborgbackup.dev" target="_blank">CyBorgBackup</a> 2020</span>
+    <span class="created-by"><a href="https://cyborgbackup.dev" target="_blank">CyBorgBackup</a> 2021</span>
     <div class="socials">
       <a href="https://github.com/cyborgbackup/cyborgbackup" target="_blank" class="ion ion-social-github"></a>
     </div>
